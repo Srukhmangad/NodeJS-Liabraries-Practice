f6f28a2345098ecaaa8a01b30bed7bf3527b457f
SRukhmangad <sumitrukhmangad@outlook.com>
2023-07-02 20:08:04 +0530

    adding files

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import ResetPassword from "../src/components/ResetPassword"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello</h1>
+     <ResetPassword></ResetPassword>  
+    </>
+  )
+}
+
+export default App
